b034d1ee40efdea7a41cdb0b39894695f42fa76a
vasikgreif <info@vaclavgreif.cz>
2021-09-09 11:37:24 +0200

    fix

--- src/index.php
+++ src/index.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace Wpify\Tracy;
-\add_action( "init", "Wpify\Tracy\init", 2 );
-function init() {
+use Wpify\Tracy\Tracy;
+
+add_action( "init", "wpify_tracy_init", 2 );
+function wpify_tracy_init() {
 	new Tracy();
 }
