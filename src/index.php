f7f005a516751e6b31376953163d9deba13b2326
Václav Greif <info@vaclavgreif.cz>
2021-12-13 18:53:02 +0100

    Load tracy once only

--- src/index.php
+++ src/index.php
@@ -3,6 +3,10 @@
 use Wpify\Tracy\Tracy;
 
 if (function_exists('add_action')) {
+	// Load only once
+	if (class_exists('\Wpify\Tracy\Tracy')) {
+		return;
+	}
 	if (defined('WPIFY_TRACY_INIT_ACTION')) {
 		add_action( "init", WPIFY_TRACY_INIT_ACTION, 2 );
 		function wpify_tracy_init() {
